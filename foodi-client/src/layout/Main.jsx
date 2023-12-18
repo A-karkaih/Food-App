ec07eff8f29842bcb3b8fde526ecfaa813676912
A-karkaih <akarkaih@gmail.com>
2023-12-18 09:25:47 +0100

    completing NavBar

---
+++ foodi-client/src/layout/Main.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { Outlet } from "react-router-dom";
+import NavBar from "../components/NavBar";
+
+const Main = () => {
+    return (
+      <div>
+            <NavBar />
+            <Outlet />
+            <footer>Footer</footer>
+      </div>
+    );
+   
+};
+
+export default Main;
